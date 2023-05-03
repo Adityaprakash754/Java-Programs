7d82623f10a6597051761b59421c38b48c814e43
Aditya Prakash <112635806+Adityaprakash754@users.noreply.github.com>
2023-05-03 22:42:25 +0530

    Delete JavaProg directory

--- JavaProg/3/Shape.java
+++
@@ -1,6 +0,0 @@
-package com.rnsit.facade;
-
-public interface Shape {
-	public void draw();
-
-}
--- JavaProg/8/account.java
+++
@@ -1,9 +0,0 @@
-package com.rnsit.bank;
-
-public interface account {
-	int year=2023;
-	public void openAccount();
-	public void minBalance();
-	public void closeAccount();
-
-}
